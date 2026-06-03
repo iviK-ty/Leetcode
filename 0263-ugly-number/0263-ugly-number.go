3655085f5dec022ff66abe288ee8df69e584f208
ivik <77621529+iviK-ty@users.noreply.github.com>
2026-06-03 19:23:29 +0900

    Time: 0 ms (100%), Space: 4.1 MB (3.54%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.go
@@ -0,0 +1,13 @@
+func isUgly(n int) bool {
+    if n <=0 {
+        return false
+    }
+    factors := []int{2,3,5}
+    for _, factor := range factors {
+        for n%factor ==0 {
+            n /= factor
+        }
+    }
+ 
+    return n == 1
+}
